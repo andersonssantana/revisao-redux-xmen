7a555bef4bf2d8d30037db77f55a9b2853bfbb7f
Anderson Santana <67117035+andersonssantana@users.noreply.github.com>
2022-08-29 10:50:56 -0300

    adiciona lint e corrige estilo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { Route, Routes as Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-import XMenList from './pages/XMenPage'
-import { Route, Routes as Switch } from 'react-router-dom';
+import XMenList from './pages/XMenPage';
 
 class App extends React.Component {
-
   render() {
     return (
       <div className="App">
-          <Switch>
-            <Route path="/" element={ <Login /> } />
-            <Route path="/search" element={ <XMenList /> } />
-          </Switch>
+        <Switch>
+          <Route path="/" element={ <Login /> } />
+          <Route path="/search" element={ <XMenList /> } />
+        </Switch>
       </div>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import LoginForm from "../components/LoginForm";
-import Title from "../components/Title";
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+import Title from '../components/Title';
 
 class Login extends React.Component {
   render() {
-    return(
+    return (
       <main className="mainLogin">
         <Title />
         <LoginForm />
